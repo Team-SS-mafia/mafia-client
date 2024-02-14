9910f16985915d3b25689bc15c925b1ebe3bb0e1
d0bee <gusdud9745@gmail.com>
2024-02-14 11:56:39 +0900

    design : add chat component

---
+++ pages/components/chatForm.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import styles from '../../styles/Chat.module.css'; // CSS 모듈 import
+import _Gstyles from '../../styles/Game.module.css'; // CSS 모듈 import
+
+const _chatForm: React.FC = () => {
+  return (
+    <div className={[_Gstyles.game_wrapper, _Gstyles.noDrag, styles.container].join(" ")}>
+      <div className={styles.messaging}>
+        <div className={styles.inbox_msg}>
+          <div className={styles.mesgs}>
+            <div className={styles.msg_history}>
+              {/* Message history items */}
+              {/* Example of incoming message */}
+              <div className={styles.incoming_msg}>
+                <div className={styles.received_msg}>
+                  <div className={styles.received_withd_msg}>
+                    <p>Test which is a new approach to have all solutions</p>
+                    <span className={styles.time_date}> TODO : date to nickname change </span>
+                  </div>
+                </div>
+              </div>
+              {/* End of message history items */}
+            </div>
+            <div className={styles.type_msg}>
+              <div className={styles.input_msg_write}>
+                <input type="text" className={styles.write_msg} placeholder="Type a message" />
+                <button className={styles.msg_send_btn} type="button"><i className="fa fa-paper-plane-o" aria-hidden="true"></i></button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+export default _chatForm;
--- pages/game.tsx
+++ pages/game.tsx
@@ -2,12 +2,20 @@ import React, { useEffect, useState } from 'react';
 import styles from '../styles/Game.module.css'; // CSS 모듈 import
 import _logo from './components/logo';
 import _gameForm from './components/gameForm';
+import _chatForm from './components/chatForm';
 
-const GameForm: React.FC = () => {
+// TODO >> 서버와 동기화 및 게임 진행 상황, 유저 요소 리로드
+// 유저 목록, 나가기 버튼, 채팅창, 내 직업 표시, + @
+// 게임 시작시 service 메시지 ex) 게임이 시작됐습니다.
+// 아침입니다, 밤입니다, 마피아가 활동할 시간입니다. 등등 표시되도록 해야함.
+// 어떻게? server side socket 통신을 통해 recv, send 과정을 통해서 진행
+
+  const GameForm: React.FC = () => {
   return (
     <div>
       <div className={[styles.game_wrapper, styles.noDrag].join(" ")} style={{minHeight: '100vh'}}>
         <_logo/>
+        <_chatForm/>
         <_gameForm/>        
       </div>
     </div>
